088ef79825a4d3e8f3181e6ddb347cd2b9dd6aaa
carlos <caustin-gonzalez@britishmuseum.org>
2019-01-21 13:19:10 +0000

    Adding utils from Pink Trombone

--- Python/utils/maths.py
+++ Python/utils/maths.py
@@ -1,7 +1,7 @@
+import random
+
 
 # Clamp a value within minimum and maximum numbers
-
-
 def clamp(value, min_value, max_value):
     if value < min_value:
         return min_value
@@ -11,8 +11,19 @@ def clamp(value, min_value, max_value):
         return value
 
 
+# Move the current value towards a target value
 def move_towards(current_value, target_value, amount):
     if current_value < target_value:
         return min(current_value + amount, target_value)
     else:
         return max(current_value - amount, target_value)
+
+
+# Unknown purpose at this stage
+def gaussian():
+    s = 0
+    c = 0
+    while c < 16:
+        c = c + 1
+        s = s + random.randint()
+    return (s - 8) / 4
---
+++ Python/vsynth/vars.py
@@ -0,0 +1,20 @@
+# Unknown purpose
+sampleRate = 0
+
+# Unknown purpose
+time = 0
+
+# Unknown purpose
+temp = {"a": 0, "b": 0}
+
+# Toggle voice on or off
+alwaysVoice = True
+
+# Toggle auto pitch wobble on or off
+autoWobble = False
+
+# Unknown purpose
+noiseFreq = 500
+
+# Unknown purpose
+noiseQ = 0.7

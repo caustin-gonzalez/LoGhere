ff3732d2547ed70359d45ce44eac8f7a5ee6b3e9
Carlos <32935225+caustin-gonzalez@users.noreply.github.com>
2019-01-25 08:21:19 +0000

    Removed mouse control functions from GUI file

--- Python/UI/synthUI.py
+++ Python/UI/synthUI.py
@@ -7,17 +7,3 @@ class SynthWindow(tkinter.Tk):
 
         self.title("LoGhere voice synthesis")
         self.geometry("600x600")
-        
-        # Unknown purpose - count the number of touches and how long it's been held down for?
-        self.touches_with_mouse = []
-        self.mouse_touch = {
-            "alive": false,
-            "end_time": 0
-        }
-        
-        # Bind left mouse-click to start_touches button
-        self.bind("<Button-1>",self.start_touches)
-        
-        # Coordinates of the mouse pointer relative to the root window.
-        self.mouseX = self.winfo_pointerx()
-        self.mouseY = self.winfo_pointery()
